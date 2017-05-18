de9edaa301f9a9352c065c459146e4ffc0f04c46
wanghuai <305420651@qq.com>
2017-05-18 21:45:55 +0800

    first

---
+++ cpp/cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by wanghuai on 2017/5/18.
+//  Copyright © 2017年 wanghuai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
