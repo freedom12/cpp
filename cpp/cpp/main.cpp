9bea51b9c9cd9d7a64ee2b3a98a2683e6609b556
wanghuai <305420651@qq.com>
2017-06-02 16:06:17 +0800

    --

--- cpp/cpp/main.cpp
+++ cpp/cpp/main.cpp
@@ -11,5 +11,19 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    std::cout << sizeof(int) << std::endl;
+    std::cout << sizeof(char) << std::endl;
+    std::cout << sizeof(long long) << std::endl;
+    
+    unsigned u = 10, u2 = 42;
+    std::cout << u2-u << std::endl;
+    std::cout << u-u2 << std::endl;
+    
+    int i = 10, i2 = 42;
+    std::cout << i2-i << std::endl;
+    std::cout << i-i2 << std::endl;
+    std::cout << i-u << std::endl;
+    std::cout << u-i << std::endl;
     return 0;
 }
